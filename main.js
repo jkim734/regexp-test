086ba5222ba1bc40610263b1171988377e30d2fd
jkim734 <jkim734@gmail.com>
2022-12-30 22:44:31 +0900

    Create Project

---
+++ main.js
@@ -0,0 +1,17 @@
+const str = `
+010-1234-5678
+thesecon@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+d
+동해물과_백두산이 마르고 닳도록`
+
+
+const h = `  the hello world  !
+
+`
+
+console.log(
+  str.match(/(?<=@).{1,}/g)
+)
